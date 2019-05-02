312dd1438a92d92ba6c0cfe297bc59595cc3f39a
CamiloJose <48070843+CamiloJose@users.noreply.github.com>
2019-05-02 14:45:09 -0600

    Add files via upload

--- Transponer matrizes Tutoria.py
+++ Transponer matrizes Tutoria.py
@@ -1,92 +1,73 @@
-"""
-#Crear una matriz de n x n en clases
-class Matriz():
-    def __init__(self):
-        pass
-    
-    def crearMatriz(self,n):
-        if isinstance(n, int) and n > 0:
-            return self.crearMatriz_aux(n, [], [], 0, 0)
-
-        else: return "AAHHHHHH"
-
-    def crearMatriz_aux(self, n, matriz, fila, indiceFila, indiceColumna):
-        if indiceFila == n:
-            return matriz
-        elif indiceColumna == n:
-            return (self.crearMatriz_aux(n, matriz + [fila], [], indiceFila + 1, 0))
-        elif indiceFila == 0 or indiceFila == (n-1):
-            return (self.crearMatriz_aux(n, matriz, fila + ["*"], indiceFila, indiceColumna + 1))
-        elif indiceColumna == 0 or indiceColumna == (n-1):
-            return (self.crearMatriz_aux(n, matriz, fila + ["*"], indiceFila, indiceColumna + 1))
-        else: return (self.crearMatriz_aux(n, matriz, fila + [0], indiceFila, indiceColumna + 1))
-
-m = Matriz()
-print("="*115)
-print(m.crearMatriz(5))
-print(" "*115)
-print("="*115)
-
-#========================================================================================
-
-#Crear una matriz de n x m en clases
-class Matriz2():
-    def __init__(self):
-        pass
-    
-    def crearMatriz(self,n, m):
-        if isinstance(n, int) and n > 0:
-            return self.crearMatriz_aux(n, m, [], [], 0, 0)
-
-        else: return "AAHHHHHH"
-
-    def crearMatriz_aux(self, n, m, matriz, fila, indiceFila, indiceColumna):
-        if indiceFila == n:
-            return matriz
-        elif indiceColumna == m:
-            return (self.crearMatriz_aux(n, m, matriz + [fila], [], indiceFila + 1, 0))
-        elif indiceFila == 0 or indiceFila == (n-1):
-            return (self.crearMatriz_aux(n, m, matriz, fila + ["*"], indiceFila, indiceColumna + 1))
-        elif indiceColumna == 0 or indiceColumna == (n-1):
-            return (self.crearMatriz_aux(n, m, matriz, fila + ["*"], indiceFila, indiceColumna + 1))
-        else: return (self.crearMatriz_aux(n, m, matriz, fila + [0], indiceFila, indiceColumna + 1))
-
-m1 = Matriz2()
-print(" "*115)
-print(m1.crearMatriz(5, 6))
-print(" "*115)
-print(">" *115)
-
-"""
-#===============================================================================
-
-
-def transponerMatriz(matriz):
-    if (isinstance(matriz, list) and matriz!=[]):
-        return transponerMatriz_aux(matriz,0,0, 0, [])
-    else: return "Error"
-
-def transponerMatriz_aux(matriz, fila, columna, x, rcolumna):
-    if fila == len(matriz):
-        return matriz
-    elif columna == len(matriz[0]):
-        return (transponerMatriz_aux(matriz, fila + 1, 0, x, rcolumna))
-    else:
-        if x == columna:
-            rcolumna = rcolumna + [matriz[fila][columna]]
-        return (transponerMatriz_aux(matriz, fila, columna + 1, x, rcolumna))
 
 #=================================================================================
 
 
 def transponer_Matriz(matriz):
     if (isinstance(matriz, list) and matriz!=[]):
-        return transponerMatriz_aux(matriz, 0, 0, 0)
+        return transponerMatriz_aux(matriz, 0, 0)
     else: return "La matriz no es una lista."
 #sumar
-def transponerMatriz_aux(matriz, fila, columna, resultado):
+def transponerMatriz_aux(matriz, fila, columna):
     if fila == len(matriz):
-        return resultado
+        return matriz
     elif columna == len(matriz[0]):
-        return transponerMatriz_aux(matriz, fila + 1, 0, resultado)
-    else: return transponerMatriz_aux(matriz, fila, columna+ 1, resultado + matriz[fila][columna])
+        return transponerMatriz_aux(matriz, fila + 1, 0)
+    else:
+        if columna > fila:
+            t = matriz[columna][fila]
+            matriz[columna][fila] = matriz[fila][columna]
+            matriz[fila][columna] = t
+        return transponerMatriz_aux(matriz, fila, columna+ 1 )
+
+
+
+
+def trans(matriz,result):
+    if (isinstance(matriz, list) and matriz!=[]):
+        return trans_copy(matriz,0,0,result)
+    else:
+        return "not list"
+
+def trans_copy(matriz,i,j,result):
+    if (i == len(matriz)):        
+        return result
+    elif (j == len(matriz[0])):
+        return trans_copy(matriz,i+1,0,result)
+    else:
+        result[j][i] = matriz[i][j]
+        return trans_copy(matriz,i,j+1,result)
+
+
+
+## creacion de una matriz
+#-------------
+def creatematriz(i,j):
+    return getmatriz(i,0,j,0, [],[])
+
+def getmatriz(i,contadori,j,contadorj, fila,matriz):
+    if contadori == i:
+        return matriz
+    elif contadorj == j:
+        matriz.append(fila)
+        return getmatriz(i,contadori+1,j,0, [],matriz)
+    else:
+        fila.append(0)
+        return getmatriz(i,contadori,j,contadorj+1, fila,matriz)
+    
+#-------------
+def create_inv(matriz):
+    filas = len(matriz)
+    cols  = len(matriz[0])
+    return creatematriz(cols,filas)
+
+matriz = [[1,2,3],[4,5,6],[7,8,9],[10,11,12]]
+result = create_inv(matriz)
+
+
+print(" ",matriz,"\n",trans(matriz, result))
+
+#________________________________
+
+
+
+
