4b447a8961bdaf9498cf351900d9f31b636a2b9e
CamiloJose <48070843+CamiloJose@users.noreply.github.com>
2019-06-14 18:59:40 -0600

    Add files via upload

---
+++ Parciale/Parcial 3 Camilo Jose Solis Gonzalez.py
@@ -0,0 +1,55 @@
+
+#1
+class Dibujar(object):
+    def __init__(self):
+        pass
+    def dibujarTriangulo(self,n,m):
+        for fila in range(n):
+            if fila ==0 or fila==(n):
+                print("*", end=" ")
+            elif columna ==0 or columna==(m):
+                print("*", end=" ")
+            else: print(" ",end=" ")
+
+#2
+"""
+Respuesta/ Agarra el numerode mayor valor de3sde la posicion mas alta y desdeahi recorre la lista cogiendo los numeros de mayor valor que el
+
+salida: 55, 44, 31, 17
+""""
+
+#3
+class Eratöstenes(object):
+    def __init__(self):
+        pass
+    def criba(self,lista):
+        Lista_primo = []
+        for indice in range(len(lista)):
+            posicion = indice
+            cambiio = lista(indice)
+            while posicion>0:
+                if (posicion%2)and(posicion%3):
+                    lista[posicion]=lista[posicion-1]
+                else:
+                    lista[posicion]==1
+                    indice.append(Lista_primo)
+            return: lista, Lista_primo
+
+#4
+class AdmicionRegistro(object):
+    def __init__(self):
+        self.escuela={}
+        self.dict={}
+    def admicion(self,estudiante,cola,paciente):
+        self.dict[estudiante]=Cola()
+        self.dict[estudiante]=cola
+        aCola=self.dict[estudiante]
+        aCola.enqueue(promedio)
+        self.dict[(estudiante,aCola)]
+    def prioridadself,estudiante):
+        self.dict[(estudiante,promedio)]
+        if(self.Cola(len(promedio))>700):
+            self.Cola.enque(self.dict[estudiante])
+
+    def matricula(self):
+        self.dict[estudiante]
